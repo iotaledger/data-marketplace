46f1eec0bd0c176df400f6c1955f12d89c3b1a9b
Lewis Freiberg <lewi@2pk.in>
2017-10-10 17:51:20 +1100

    Init Data stream

---
+++ components/data-stream/index.js
@@ -0,0 +1,58 @@
+import React from "react"
+import styled from "styled-components"
+import SensorCard from "../sensor-card"
+
+const InfoCol = styled.main`
+  position: relative;
+  width: 880px;
+  padding-left: 30px;
+  @media (max-width: 760px) {
+    width: 100%;
+    padding: 0;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 455px;
+    width: 1px;
+    height: 100%;
+    background-color: #738fd4;
+    @media (max-width: 1195px) {
+      left: 30px;
+    }
+    @media (max-width: 760px) {
+      visibility: hidden;
+    }
+  }
+`
+const CardWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  padding: 40px 0 100px;
+  @media (max-width: 1195px) {
+    flex-flow: column nowrap;
+    padding-bottom: 0;
+    margin-left: 30px;
+  }
+  @media (max-width: 760px) {
+    width: 100%;
+    margin-left: 0;
+    align-items: center;
+  }
+`
+
+export default class extends React.Component {
+  render() {
+    return (
+      <InfoCol>
+        <CardWrapper>
+          {Array(10)
+            .fill()
+            .map((_, i) => <SensorCard index={i} key={i} />)}
+        </CardWrapper>
+      </InfoCol>
+    )
+  }
+}
