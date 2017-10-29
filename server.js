5d0a47f716384273e72208494f774f6ab67f8dfd
Lewis Freiberg <lewi@2pk.in>
2017-10-29 23:54:17 +1100

    Add in server routes

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const port = parseInt(process.env.PORT, 10) || 3000
+
 const next = require('next')
 const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
 
+const Firebase = require('./lib/serverDB')
 
-// Without express
-const {createServer} = require('http')
 app.prepare().then(() => {
-  createServer(handler).listen(3000)
+  const server = express()
+  server.use(bodyParser.json())
+
+  server.post('/purchase', (req, res) => {
+    console.log(req.body)
+    var packet = req.body
+    if (packet.full) {
+      Firebase.full(packet.id, packet.device)
+    } else {
+      Firebase.partial(packet.id, packet.device)
+    }
+    return true
+  })
+
+  server.get('*', (req, res) => {
+    return handler(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
