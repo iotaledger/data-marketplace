d30e0c7119bde88711369ea793d8b3f70777bdcc
Lewis Freiberg <lewi@2pk.in>
2017-11-28 00:11:13 +0800

    Logos

--- components/partners/index.js
+++ components/partners/index.js
@@ -8,71 +8,169 @@ export default props => (
       <Ul>
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/accenture.png"
+            srcSet="/static/logotypes/accenture.png 2x"
             alt="Logo"
           />
         </Li>
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/agder.png"
+            srcSet="/static/logotypes/agder.png 2x"
             alt="Logo"
           />
         </Li>
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/apgsga.png"
+            srcSet="/static/logotypes/apgsga.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/blocklab.png"
+            srcSet="/static/logotypes/blocklab.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/bosch.png"
+            srcSet="/static/logotypes/bosch.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/dnvgl.png"
+            srcSet="/static/logotypes/dnvgl.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/elering.png"
+            srcSet="/static/logotypes/elering.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/engie.png"
+            srcSet="/static/logotypes/engie.png 2x"
+            alt="Logo"
+          />
+        </Li>{' '}
+        <Li>
+          <I
+            src="/static/logotypes/ewe.png"
+            srcSet="/static/logotypes/ewe.png 2x"
             alt="Logo"
           />
         </Li>
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/farmforce.png"
+            srcSet="/static/logotypes/farmforce.png 2x"
             alt="Logo"
           />
         </Li>{' '}
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/fujitsu.png"
+            srcSet="/static/logotypes/fujitsu.png 2x"
             alt="Logo"
           />
         </Li>{' '}
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/ice.png"
+            srcSet="/static/logotypes/ice.png 2x"
             alt="Logo"
           />
         </Li>{' '}
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/ismb.png"
+            srcSet="/static/logotypes/ismb.png 2x"
             alt="Logo"
           />
         </Li>{' '}
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/movimento.png"
+            srcSet="/static/logotypes/movimento.png 2x"
             alt="Logo"
           />
         </Li>{' '}
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/ntnu.png"
+            srcSet="/static/logotypes/ntnu.png 2x"
             alt="Logo"
           />
-        </Li>{' '}
+        </Li>
         <Li>
           <I
-            src="/static/logotypes/generic.png"
-            srcSet="/static/logotypes/generic.png 2x"
+            src="/static/logotypes/orange.png"
+            srcSet="/static/logotypes/orange.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/oslo.png"
+            srcSet="/static/logotypes/oslo.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/poyry.png"
+            srcSet="/static/logotypes/poyry.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/samsung.png"
+            srcSet="/static/logotypes/samsung.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/schneider.png"
+            srcSet="/static/logotypes/schneider.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/trianel.png"
+            srcSet="/static/logotypes/trianel.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/trondheim.png"
+            srcSet="/static/logotypes/trondheim.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/tum.png"
+            srcSet="/static/logotypes/tum.png 2x"
+            alt="Logo"
+          />
+        </Li>
+        <Li>
+          <I
+            src="/static/logotypes/uio.png"
+            srcSet="/static/logotypes/uio.png 2x"
             alt="Logo"
           />
         </Li>
@@ -92,7 +190,7 @@ const S = styled.section`
 const C = styled.div`
   transform: skewY(2deg);
   width: 100%;
-  max-width: 1170px;
+  max-width: 1440px;
   padding: 0 15px;
   margin-right: auto;
   margin-left: auto;
@@ -142,6 +240,10 @@ const Li = styled.li`
   }
 `
 const I = styled.img`
+  max-height: 120px;
   max-width: 200px;
-  padding: 5px;
+  padding: 10px 15px;
+  @media (max-width: 760px) {
+    max-width: 150px;
+  }
 `
