6862807d33b8be9baafb5b52a05ea0f0af646345
Lewis Freiberg <lewi@2pk.in>
2017-11-30 21:17:26 +0800

    Update route and gun

--- server.js
+++ server.js
@@ -8,25 +8,57 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
 
 const Firebase = require('./lib/serverDB')
+var mailgun = require('mailgun.js')
+
+var mg = mailgun.client({
+  username: 'api',
+  key: 'key-1104febc601341ced88b1fa1598a5e72',
+  public_key: 'pubkey-b573386b5632c7e2dd7b9447cc00dfe6'
+})
 
 app.prepare().then(() => {
   const server = express()
   server.use(bodyParser.json())
 
-  server.post('/purchase', async (req, res) => {
+  server.post('/email', async (req, res) => {
     var packet = req.body
     console.log(packet)
-    if (!packet || !packet.id || !packet.device)
+    if (
+      !packet ||
+      !packet.name ||
+      !packet.company ||
+      !packet.email ||
+      !packet.body ||
+      !packet.captcha
+    )
       return res.json('Malformed Request')
 
-    if (packet.full) {
-      var resp = await Firebase.full(packet.id, packet.device)
-      console.log(resp)
-      return res.json(JSON.stringify(resp))
-    } else {
-      var resp = await Firebase.partial(packet.id, packet.device)
-      return res.json(JSON.stringify(resp))
-    }
+    var cap = await fetch(
+      `https://www.google.com/recaptcha/api/siteverify?secret=6LeIFTsUAAAAAEuditG8maUke__qA9h2NywlQIBv&response=${
+        packet.captcha
+      }`,
+      {
+        method: 'POST'
+      }
+    )
+    var resp = await cap.json()
+    if (!resp.success) return res.json({ error: 'Malformed Request' })
+
+    mg.messages
+      .create('mail.tangle.works', {
+        from: 'Data Market <mailgun@mail.tangle.works>',
+        to: ['particpate@iota.org'],
+        subject: 'Marketplace Form Inquiry',
+        html: `<h1>Marketplace</h1>
+        <div>
+          <p><strong>Name:</strong></p><p>${packet.name}</p>  
+          <p><strong>Company:</strong></p><p>${packet.company}</p>
+          <p><strong>Email:</strong></p><p>${packet.email}</p>
+          <p><strong>Message:</strong></p><p>${packet.body}</p>
+        </div>`
+      })
+      .then(msg => res.json({ success: true })) // logs response data
+      .catch(err => console.log(err)) // logs any error
   })
 
   server.get('*', (req, res) => {
