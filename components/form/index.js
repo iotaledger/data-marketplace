37871dc72b3891ab554c470c9867a3e655ebc9e3
Lewis Freiberg <lewi@2pk.in>
2017-11-30 21:17:12 +0800

    Update Form

--- components/form/index.js
+++ components/form/index.js
@@ -3,47 +3,145 @@ import styled from 'styled-components'
 import Recaptcha from 'react-recaptcha'
 
 export default class extends React.Component {
-  state = { name: '', email: '', body: '', captcha: null }
+  state = {
+    name: '',
+    email: '',
+    body: '',
+    company: '',
+    captcha: null,
+    loading: false,
+    success: false
+  }
 
   verify = data => {
     this.setState({ captcha: data })
   }
 
+  submit = async e => {
+    let state = this.state
+    if (state.loading) return
+    if (!state.name || !state.email || !state.body)
+      this.setState({ loading: true }, async () => {
+        var response = await fetch('/email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(state)
+        })
+        var data = await response.json()
+        this.setState({ success: true })
+      })
+    e.preventDefault()
+  }
+
   render() {
+    var { name, email, body, company, success } = this.state
     return (
       <S>
         <C>
           <H>Get Involved</H>
         </C>
-        <C>
-          <input type={'text'} />
-          <input type={'email'} />
-          <textarea />
-          <Recaptcha
-            sitekey="6LeIFTsUAAAAAHRqa-Y9JtoN8Bopd3gQBDM2ItCm"
-            verifyCallback={this.verify}
-          />,
-        </C>
+        {!success ? (
+          <F onSubmit={() => this.submit}>
+            <I
+              type={'text'}
+              placeholder={'Your Full Name'}
+              value={name}
+              onChange={e => this.setState({ name: e.target.value })}
+            />
+
+            <I
+              type={'email'}
+              placeholder={'Your email'}
+              value={email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+            <I
+              type={'text'}
+              placeholder={'Your Company'}
+              value={company}
+              onChange={e => this.setState({ company: e.target.value })}
+            />
+            <T
+              value={body}
+              placeholder={'Your message here...'}
+              onChange={e => this.setState({ body: e.target.value })}
+            />
+            <Recaptcha
+              sitekey="6LeIFTsUAAAAAHRqa-Y9JtoN8Bopd3gQBDM2ItCm"
+              verifyCallback={this.verify}
+            />
+            {!this.state.loading && <Button type={'submit'}>Submit</Button>}
+            {this.state.loading && <Button>Sending</Button>}
+          </F>
+        ) : (
+          <C>
+            <H>Submitted</H>
+          </C>
+        )}
+
+        <Bottom />
       </S>
     )
   }
 }
 
+const Bottom = styled.div`
+  position: absolute;
+  height: 50px;
+`
+const F = styled.form`
+  display: flex;
+  margin: 0 auto;
+  padding: 0 10px;
+  flex-direction: column;
+  align-items: center;
+  max-width: 400px;
+`
+
+const I = styled.input`
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 9px 14px;
+  font-size: 105%;
+  margin: 30px 0 0;
+  width: 100%;
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.15);
+  }
+`
+const T = styled.textarea`
+  height: 100px;
+  width: 100%;
+  font-size: 105%;
+  padding: 9px 14px;
+  margin: 30px 0;
+  border: none;
+  background: transparent;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.15);
+  }
+`
+
 const S = styled.section`
   background-image: linear-gradient(-189deg, #eaf0f4 1%, #f3f8fa 95%);
   padding: 90px 0 70px;
-  transform: skewY(-2deg);
+  /* transform: skewY(-2deg); */
   @media (max-width: 760px) {
     padding-bottom: 45px;
   }
 `
 const C = styled.div`
-  transform: skewY(2deg);
+  /* transform: skewY(2deg); */
   width: 100%;
   max-width: 1440px;
   padding: 0 15px;
   margin-right: auto;
   margin-left: auto;
+  margin-top: 30px;
 `
 const H = styled.p`
   font-size: 13px;
@@ -89,11 +187,21 @@ const Li = styled.li`
     margin-bottom: 40px;
   }
 `
-const I = styled.img`
-  max-height: 120px;
-  max-width: 200px;
-  padding: 10px 15px;
-  @media (max-width: 760px) {
-    max-width: 150px;
-  }
+const Button = styled.button`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  font: 15px 'Nunito Sans', sans-serif;
+  letter-spacing: 0.47px;
+  padding: 20px 38px;
+  border-radius: 100px;
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 12px;
+  letter-spacing: 0.38px;
+  padding: 12px 21px;
+  margin: 30px 0 0;
+  box-shadow: 0 10px 20px 0 rgba(10, 32, 86, 0.4);
+  font-weight: 700;
+  background-color: #009fff;
 `
