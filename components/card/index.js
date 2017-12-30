10a09b8f40020aa0028660237857f7f13d57352a
Lewis Freiberg <lewi@2pk.in>
2017-12-30 14:20:38 +0800

    Canablise the card component to be more flexible

---
+++ components/card/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+export default props => (
+  <Card data-component="SensorCard">
+    {console.log(props)}
+    {props.header && <CardHeader>{props.header}</CardHeader>}
+    {props.children}
+    {props.footer && <CardFooter>{props.footer}</CardFooter>}
+  </Card>
+)
+
+const Card = styled.a`
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  padding-top: 20px;
+  border: 1px solid #eaecee;
+  border-radius: 6px;
+  margin-bottom: 40px;
+  background-color: #fff;
+  cursor: pointer;
+  transition: box-shadow 0.19s ease-out;
+  @media (max-width: 1120px) {
+    margin-bottom: 20px;
+  }
+  @media (max-width: 400px) {
+    width: 280px;
+  }
+  &:hover {
+    box-shadow: 0 23px 50px 0 rgba(25, 54, 80, 0.1);
+  }
+`
+const CardHeader = styled.header`
+  position: relative;
+  padding: 0 30px 8px 30px;
+  border-bottom: 1px solid #eaecee;
+`
+
+const CardFooter = styled.footer`
+  padding: 20px 30px;
+  background-color: rgba(206, 218, 226, 0.2);
+  border-top: 1px solid #eaecee;
+`
