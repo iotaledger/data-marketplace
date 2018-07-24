1e2f9af279a3085adb44a75db42c60f9e1ce0dde
Alexey Sobolev <sobolev@mail.de>
2018-07-24 15:27:55 +0200

    Refactoring using Redux

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/home';
 import SensorPage from './pages/sensor';
 import CookiesPage from './pages/cookies';
 import DashboardPage from './pages/dashboard';
 import WhitelistPage from './pages/whitelist';
+import { storeProjectSettings } from './store/settings/actions';
 
-const Router = () => (
-  <Switch>
-    <Route path="/" component={HomePage} exact />
-    <Route path="/dashboard" component={DashboardPage} />
-    <Route path="/sensor/:id" component={SensorPage} />
-    <Route path="/cookies" component={CookiesPage} />
-    <Route path="/whitelist" component={WhitelistPage} />
-    <Route component={HomePage} />
-  </Switch>
-);
+class Router extends Component {
+  componentDidMount() {
+    this.props.storeProjectSettings();
+  }
 
-export default Router;
+  render() {
+    return (
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/dashboard" component={DashboardPage} />
+          <Route path="/sensor/:id" component={SensorPage} />
+          <Route path="/cookies" component={CookiesPage} />
+          <Route path="/whitelist" component={WhitelistPage} />
+          <Route component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  storeProjectSettings: () => dispatch(storeProjectSettings()),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Router);
