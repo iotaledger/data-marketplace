1e22fb527b158addf7f9c392088df7f7b56ad170
Alexey Sobolev <sobolev@mail.de>
2018-07-30 17:06:05 +0200

    Refactored components

--- src/components/inview/index.js
+++ src/components/inview/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import InView from 'in-view';
 
-export default class extends React.Component {
-  state = {};
-  componentDidMount = () => {
+export default class extends Component {
+  componentDidMount() {
     InView('.inview').on('enter', el => this.props.func());
-  };
+  }
 
   render() {
     return <div className={'inview'}>{this.props.children}</div>;
