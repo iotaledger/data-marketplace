a610f5a64a8d1051c2e606f495defb134c62bee5
Alexey Sobolev <sobolev@mail.de>
2018-07-31 10:39:12 +0200

    Refactored pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-// import dynamic from 'next/dynamic';
-
+import firebase from 'firebase/app';
 import Header from '../components/header';
 import Features from '../components/feature-section';
 import SensorList from '../components/sensor-list';
@@ -10,13 +9,7 @@ import Benefits from '../components/benefits';
 import Form from '../components/form';
 import Footer from '../components/footer';
 import Map from '../components/map';
-
-import FB from '../utils/firebase';
-import { allDevices } from '../utils/auth-user';
-
-// const Map = dynamic(import('../components/map'), {
-//   ssr: false,
-// });
+import { allDevices } from '../utils/firebase';
 
 export default class extends React.Component {
   state = {
@@ -26,8 +19,6 @@ export default class extends React.Component {
   };
 
   componentDidMount = async () => {
-    // Firebase
-    const firebase = await FB();
     const devices = await allDevices(firebase);
     this.setState({ devices, loading: false });
   };
