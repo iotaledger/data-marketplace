af2d531b48cfa799aa280af78b9a3f48127c5e93
Alexey Sobolev <sobolev@mail.de>
2018-09-06 18:09:27 +0200

    Refactored template generator

--- public/static/template/index.js
+++ public/static/template/index.js
@@ -4,7 +4,7 @@ const { debug, serverUrl } = require('./config.json');
 const data = require('./data.json');
 
 // EXAMPLE 1: read static data from file/database
-data.map(payload => {
+data.forEach(payload => {
   if (debug) {
     console.log(payload);
   } else {
@@ -14,21 +14,33 @@ data.map(payload => {
 });
 
 // EXAMPLE 2: request data from server or sensor
-Array.from(Array(10), async () => {
-  // Access public server. URL is configurable in config.json
-  // In this example a Star Wars API is used for demo purposes
+const queryData = async delay => {
   const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
-  const resp = await fetch(serverUrl + getRandomInt(73)); // construct URL to request a random star wars vehicle
-  const json = await resp.json();
-  if (!(json.detail && json.detail === 'Not found')) {
-    const { name, model, manufacturer, vehicle_class } = json;
-    const payload = { name, model, manufacturer, vehicle_class };
+  while (true) {
+    // Access public server. URL is configurable in config.json
+    // In this example a Star Wars API is used for demo purposes
+    const response = await fetch(serverUrl + getRandomInt(73)); // construct URL to request a random star wars vehicle
+    const status = response.status;
+    const json = await response.json();
 
-    if (debug) {
-      console.log(payload);
-    } else {
-      // Publish sensor data to marketplace
-      publish(payload); // your sensor data goes here. Payload is any content in JSON format
+    // Continuously check the response status until it's completed, failed or times out.
+    if (status === 418) { // specify your HTTP status codes here
+      break;
+    } else if (status === 200) {
+      const { name, model, manufacturer, vehicle_class } = json;
+      const payload = { name, model, manufacturer, vehicle_class };
+
+      if (debug) {
+        console.log(payload);
+      } else {
+        // Publish sensor data to marketplace
+        await publish(payload); // your sensor data goes here. Payload is any content in JSON format
+      }
+
+      // Delay before running the next loop iteration:
+      await new Promise(resolve => setTimeout(resolve, delay));
     }
   }
-});
+}
+
+queryData(60000) // query data every 60 seconds
