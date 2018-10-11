315bd2f7de489eaa29c697cc2d454f178069cb3d
Alexey Sobolev <sobolev@mail.de>
2018-10-11 13:26:46 +0200

    Old landing page, new backend

---
+++ src/pages/home_old.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/header/old';
+import Features from '../components/feature-section/old';
+import SensorList from '../components/sensor-list';
+import Partners from '../components/partners/old';
+import Benefits from '../components/benefits/old';
+import Footer from '../components/footer/old';
+import Map from '../components/map';
+import { allDevices } from '../utils/firebase';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      devices: [],
+      loading: true,
+      anchor: null,
+    };
+
+    this.onAnchorClick = this.onAnchorClick.bind(this);
+  }
+
+  async componentDidMount() {
+    const devices = await allDevices();
+    this.setState({ devices, loading: false });
+  };
+
+  onAnchorClick(anchor) {
+    this.setState({ anchor });
+  }
+
+  render() {
+    return (
+      <Main>
+        <Header onAnchorClick={this.onAnchorClick} />
+        <Features />
+        <Map {...this.state} />
+        <SensorList {...this.state} />
+        <Partners {...this.state} />
+        <Benefits />
+        <Footer />
+      </Main>
+    );
+  }
+}
+
+const Main = styled.div`
+  overflow-x: hidden;
+`;
